5d9449b7264ad1eac8f4f2518556cc8bab1bdf70
Brandon Baars <brandonbaars@gmail.com>
2020-01-22 23:29:32 -0500

    Initial Commit

---
+++ SoundVisualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundVisualizer
+//
+//  Created by Brandon Baars on 1/22/20.
+//  Copyright © 2020 Brandon Baars. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
